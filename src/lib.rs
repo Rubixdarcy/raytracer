c1d0a4142a646d3ce180a5e35d678e357748b437
Darcy Ryan <darcy150@gmail.com>
2020-12-28 22:45:30 +1100

    Transformations

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,12 @@ pub mod tuple;
 pub mod color;
 pub mod canvas;
 pub mod matrix;
+pub mod transformations;
 
 pub mod prelude {
     pub use crate::tuple::{tuple, point, vector, T4};
     pub use crate::color::{color, Color};
     pub use crate::canvas::{Canvas};
     pub use crate::matrix::{M2, M3, M4, matrix2, matrix3, matrix4};
+    pub use crate::transformations::{translation, scaling, rotation_x, rotation_y, rotation_z, shearing};
 }
---
+++ src/transformations.rs
@@ -0,0 +1,181 @@
+use crate::prelude::*;
+
+pub fn translation(x: f32, y: f32, z: f32) -> M4 {
+    matrix4(1.0, 0.0, 0.0, x,
+            0.0, 1.0, 0.0, y,
+            0.0, 0.0, 1.0, z,
+            0.0, 0.0, 0.0, 1.0,)
+}
+
+pub fn scaling(x: f32, y: f32, z: f32) -> M4 {
+    matrix4(x,   0.0, 0.0, 0.0,
+            0.0, y,   0.0, 0.0,
+            0.0, 0.0, z,   0.0,
+            0.0, 0.0, 0.0, 1.0,)
+}
+
+pub fn rotation_x(theta: f32) -> M4 {
+    let (sin, cos) = theta.sin_cos();
+    matrix4(1.0, 0.0, 0.0,  0.0,
+            0.0, cos, -sin, 0.0,
+            0.0, sin, cos,  0.0,
+            0.0, 0.0, 0.0,  1.0)
+}
+
+pub fn rotation_y(theta: f32) -> M4 {
+    let (sin, cos) = theta.sin_cos();
+    matrix4(cos,  0.0, sin, 0.0,
+            0.0,  1.0, 0.0, 0.0,
+            -sin, 0.0, cos, 0.0,
+            0.0,  0.0, 0.0, 1.0,)
+}
+
+pub fn rotation_z(theta: f32) -> M4 {
+    let (sin, cos) = theta.sin_cos();
+    matrix4(cos, -sin, 0.0, 0.0,
+            sin, cos,  0.0, 0.0,
+            0.0, 0.0,  1.0, 0.0,
+            0.0, 0.0,  0.0, 1.0,)
+}
+
+pub fn shearing(xy: f32, xz: f32, yx: f32, yz: f32, zx: f32, zy: f32) -> M4 {
+    matrix4(1.0, xy,  xz,  0.0,
+            yx,  1.0, yz,  0.0,
+            zx,  zy,  1.0, 0.0,
+            0.0, 0.0, 0.0, 1.0,)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::f32::consts::{FRAC_1_SQRT_2, FRAC_PI_2, FRAC_PI_4};
+
+    //macro_rules! assert_approx_eq {
+    //    ($x:expr, $y:expr)  => (assert!(approx_eq!(f32, $x, $y, epsilon = 0.00001)))
+    //}
+
+    #[test]
+    fn translation_multiply() {
+        assert_eq!(translation(5.0, -3.0, 2.0) * point(-3.0, 4.0, 5.0),
+                   point(2.0, 1.0, 7.0));
+    }
+
+    #[test]
+    fn translation_inverse() {
+        assert_eq!(translation(5.0, -3.0, 2.0).inverse() * point(-3.0, 4.0, 5.0),
+                   point(-8.0, 7.0, 3.0));
+    }
+
+    #[test]
+    fn translation_vector_no_effect() {
+        let v = vector(-3.0, 4.0, 5.0);
+        assert_eq!(translation(5.0, -3.0, 2.0) * v, v);
+    }
+    
+    #[test]
+    fn scaling_point() {
+        assert_eq!(scaling(2.0, 3.0, 4.0) * point(-4.0, 6.0, 8.0),
+                   point(-8.0, 18.0, 32.0));
+    }
+    
+    #[test]
+    fn scaling_vector() {
+        assert_eq!(scaling(2.0, 3.0, 4.0) * vector(-4.0, 6.0, 8.0),
+                   vector(-8.0, 18.0, 32.0));
+    }
+    
+    #[test]
+    fn scaling_inverse() {
+        assert_eq!(scaling(2.0, 3.0, 4.0).inverse() * vector(-4.0, 6.0, 8.0),
+                   vector(-2.0, 2.0, 2.0));
+    }
+
+    #[test]
+    fn scaling_reflection() {
+        assert_eq!(scaling(-1.0, 1.0, 1.0) * point(2.0, 3.0, 4.0),
+                   point(-2.0, 3.0, 4.0));
+    }
+
+    #[test]
+    fn rotation_x_axis() {
+        let p = point(0.0, 1.0, 0.0);
+        assert_eq!(rotation_x(FRAC_PI_4) * p, point(0.0, FRAC_1_SQRT_2, FRAC_1_SQRT_2));
+        assert_eq!(rotation_x(FRAC_PI_2) * p, point(0.0, 0.0, 1.0));
+    }
+
+    #[test]
+    fn rotation_x_inverse() {
+        assert_eq!(rotation_x(FRAC_PI_4).inverse() * point(0.0, 1.0, 0.0),
+                   point(0.0, FRAC_1_SQRT_2, -FRAC_1_SQRT_2));
+    }
+
+    #[test]
+    fn rotation_y_axis() {
+        let p = point(0.0, 0.0, 1.0);
+        assert_eq!(rotation_y(FRAC_PI_4) * p, point(FRAC_1_SQRT_2, 0.0, FRAC_1_SQRT_2));
+        assert_eq!(rotation_y(FRAC_PI_2) * p, point(1.0, 0.0, 0.0));
+    }
+    
+    #[test]
+    fn rotation_z_axis() {
+        let p = point(0.0, 1.0, 0.0);
+        assert_eq!(rotation_z(FRAC_PI_4) * p, point(-FRAC_1_SQRT_2, FRAC_1_SQRT_2, 0.0));
+        assert_eq!(rotation_z(FRAC_PI_2) * p, point(-1.0, 0.0, 0.0));
+    }
+
+    #[test]
+    fn shearing_all() {
+        let p = point(2.0, 3.0, 4.0);
+        assert_eq!(shearing(1.0, 0.0, 0.0, 0.0, 0.0, 0.0) * p, point(5.0, 3.0, 4.0));
+        assert_eq!(shearing(0.0, 1.0, 0.0, 0.0, 0.0, 0.0) * p, point(6.0, 3.0, 4.0));
+        assert_eq!(shearing(0.0, 0.0, 1.0, 0.0, 0.0, 0.0) * p, point(2.0, 5.0, 4.0));
+        assert_eq!(shearing(0.0, 0.0, 0.0, 1.0, 0.0, 0.0) * p, point(2.0, 7.0, 4.0));
+        assert_eq!(shearing(0.0, 0.0, 0.0, 0.0, 1.0, 0.0) * p, point(2.0, 3.0, 6.0));
+        assert_eq!(shearing(0.0, 0.0, 0.0, 0.0, 0.0, 1.0) * p, point(2.0, 3.0, 7.0));
+    }
+
+    #[test]
+    fn transformation_sequence() {
+        let p = point(1.0, 0.0, 1.0);
+        let a = rotation_x(FRAC_PI_2);
+        let b = scaling(5.0, 5.0, 5.0);
+        let c = translation(10.0, 5.0, 7.0);
+
+        assert_eq!((c * b * a) * p, (c * (b * (a * p))));
+        assert_eq!((c * b * a) * p, point(15.0, 0.0, 7.0));
+    }
+}
+/*
+TODO
+Scenario: The transformation matrix for the default orientation
+  Given from â† point(0, 0, 0)
+    And to â† point(0, 0, -1)
+    And up â† vector(0, 1, 0)
+  When t â† view_transform(from, to, up)
+  Then t = identity_matrix
+
+Scenario: A view transformation matrix looking in positive z direction
+  Given from â† point(0, 0, 0)
+    And to â† point(0, 0, 1)
+    And up â† vector(0, 1, 0)
+  When t â† view_transform(from, to, up)
+  Then t = scaling(-1, 1, -1)
+
+Scenario: The view transformation moves the world
+  Given from â† point(0, 0, 8)
+    And to â† point(0, 0, 0)
+    And up â† vector(0, 1, 0)
+  When t â† view_transform(from, to, up)
+  Then t = translation(0, 0, -8)
+
+Scenario: An arbitrary view transformation
+  Given from â† point(1, 3, 2)
+    And to â† point(4, -2, 8)
+    And up â† vector(1, 1, 0)
+  When t â† view_transform(from, to, up)
+  Then t is the following 4x4 matrix:
+      | -0.50709 | 0.50709 |  0.67612 | -2.36643 |
+      |  0.76772 | 0.60609 |  0.12122 | -2.82843 |
+      | -0.35857 | 0.59761 | -0.71714 |  0.00000 |
+      |  0.00000 | 0.00000 |  0.00000 |  1.00000 |
+*/
